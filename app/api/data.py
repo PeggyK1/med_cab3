0d81a30f520764740d21eaabe828fd4b7efc9354
PeggyK1 <peggy.krom@protonmail.com>
2020-08-26 11:35:13 -0600

    Addind data

--- app/api/data.py
+++ app/api/data.py
@@ -1,9 +1,19 @@
 from os import getenv
 import pandas as pd
+from fastapi import APIRouter
 
 
-REPO_FILEPATH = getenv('REPO_FILEPATH')
-strain = pd.read_csv(REPO_FILEPATH + '\data\final_strains.csv')
+
+@router.post('/data')
+async def data():
+    """
+    Return cannabis data as json
+    """
+
+    REPO_FILEPATH = getenv('REPO_FILEPATH')
+    strain = pd.read_csv(REPO_FILEPATH + '\data\final_strains.csv')
 
 
-results = strain.to_json(orient="index")
+    results = strain.to_json(orient="index")
+
+
