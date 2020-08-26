90129eaecaf3e4fa270145c8835ff48f156980dd
PeggyK1 <peggy.krom@protonmail.com>
2020-08-26 14:57:07 -0600

    data

--- app/api/data.py
+++ app/api/data.py
@@ -10,11 +10,11 @@ router = APIRouter()
 
 REPO_FILEPATH = getenv('REPO_FILEPATH')
 strain = pd.read_csv(REPO_FILEPATH + '\data\strains.csv')
-results = strain.to_json(orient="index")
+result = strain.to_json(orient="index")
 
 @router.get('/data')
 async def data():
     """
     Return cannabis data as json
     """
-    return results
+    return result
